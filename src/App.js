901fbe66fece4081220c719bf2e95586de91eb91
Nandini Tiwari <98204182+nandini601@users.noreply.github.com>
2023-07-22 14:30:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,147 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Navbar = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <header className="text-gray-400 bg-gray-900 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+          <span className="ml-3 text-xl">XYZ</span>
         </a>
-      </header>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <a href="home.html">
+            <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+              Home
+            </button>
+          </a>
+
+          <a href="download.html">
+            <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+              Download
+            </button>
+          </a>
+
+          <a href="documentation.html">
+            <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+              Documentation
+            </button>
+          </a>
+
+          <a href="help.html">
+            <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+              Help
+            </button>
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+const HeroSection = () => {
+  return (
+    <div className="heroSection">
+      <div className="heroText">
+        <h1>You can do it!</h1>
+        <p>Push yourself, no one else is going to do it for you.</p>
+        <button className="joinNow">Join now</button>
+        <button>Know more</button>
+      </div>
     </div>
   );
-}
+};
+
+const DownloadLink = () => {
+  return (
+    <section className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
+          <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-white">
+            Download Link
+          </h1>
+          <a href="download.html">
+            <button className="flex-shrink-0 text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg mt-10 sm:mt-0">
+              Download
+            </button>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="text-gray-400 bg-gray-900 body-font">
+      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+          <span className="ml-3 text-xl">XYZ</span>
+        </a>
+        <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">
+          © 2023 XYZ —
+          <a
+            href="https://twitter.com/knyttneve"
+            className="text-gray-500 ml-1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            HashItOut
+          </a>
+        </p>
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+          <a className="text-gray-400">
+            <svg
+              fill="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              strokeWidth="2"
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+            </svg>
+          </a>
+          {/* Add other social media links */}
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <DownloadLink />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
